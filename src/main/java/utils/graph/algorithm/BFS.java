96af822209de15956f4737f4741e818cfbff228d
luigidb <luigi.debianchi@gmail.com>
2019-05-13 17:30:41 +0200

    Added bfs on adjacency list

--- src/main/java/utils/graph/algorithm/BFS.java
+++ src/main/java/utils/graph/algorithm/BFS.java
@@ -11,19 +11,20 @@ public class BFS<T> {
     private Map<T, List<IEdge<T>>> mapGraph;
 
     public BFS(int[][] matrixGraph, T[] nodes) {
-        if(matrixGraph.length != nodes.length || matrixGraph[0].length != nodes.length) {
+        if (matrixGraph.length != nodes.length || matrixGraph[0].length != nodes.length) {
             throw new RuntimeException();
         }
         this.matrixGraph = matrixGraph;
         this.nodes = nodes;
     }
 
-    public BFS(Map<T, List<IEdge<T>>> mapGraph) {
+    public BFS(Map<T, List<IEdge<T>>> mapGraph, T[] nodes) {
         this.mapGraph = mapGraph;
+        this.nodes = nodes;
     }
 
     public List<T> visit(T startNode) {
-        if(mapGraph == null) {
+        if (mapGraph == null) {
             return matrixVisit(startNode);
         } else {
             return graphVisit(startNode);
@@ -38,18 +39,18 @@ public class BFS<T> {
         Queue<T> bfsQueue = new LinkedList<>();
         bfsQueue.add(startNode);
 
-        while(!bfsQueue.isEmpty()) {
+        while (!bfsQueue.isEmpty()) {
             T poll = bfsQueue.poll();
             visitOrder.add(poll);
 
             int index = Arrays.asList(nodes).indexOf(poll);
             for (int i = 0; i < matrixGraph[index].length; i++) {
-                if(matrixGraph[index][i] != 0) {
+                if (matrixGraph[index][i] != 0) {
                     T nextVisit = nodes[i];
                     if (toBeVisited.contains(nextVisit)) {
-                       toBeVisited.remove(nextVisit);
-                       bfsQueue.add(nextVisit);
-                   }
+                        toBeVisited.remove(nextVisit);
+                        bfsQueue.add(nextVisit);
+                    }
                 }
             }
         }
@@ -58,6 +59,28 @@ public class BFS<T> {
     }
 
     private List<T> graphVisit(T startNode) {
-        return null;
+        List<T> visitOrder = new LinkedList<>();
+        Set<T> toBeVisited = new HashSet<>(Arrays.asList(nodes));
+        Queue<T> bfsQueue = new LinkedList<>();
+
+        toBeVisited.remove(startNode);
+        bfsQueue.add(startNode);
+
+        while (!bfsQueue.isEmpty()) {
+            T poll = bfsQueue.poll();
+            visitOrder.add(poll);
+
+            List<IEdge<T>> edges = mapGraph.get(poll);
+            if (edges != null)
+                for (IEdge<T> edge : edges) {
+                    T nodeF = edge.getNodeF();
+                    if (toBeVisited.contains(nodeF)) {
+                        toBeVisited.remove(nodeF);
+                        bfsQueue.add(nodeF);
+                    }
+                }
+        }
+
+        return visitOrder;
     }
 }
--- src/test/java/utils/graph/algorithm/BFSTest.java
+++ src/test/java/utils/graph/algorithm/BFSTest.java
@@ -1,18 +1,17 @@
 package utils.graph.algorithm;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import utils.graph.egde.IEdge;
+import utils.graph.egde.directed.WeightedDirectedEdge;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class BFSTest {
 
     private BFS<Character> bfs;
 
-    @Before
-    public void setUp() {
+    private void initMatrixGraph() {
         int[][] graph = new int[][] {
                 {0, 10, 10, 0,  0,  0},
                 {0, 0,  0,  20, 5,  0},
@@ -25,19 +24,64 @@ public class BFSTest {
     }
 
     @Test
-    public void testBFSOnALeaf() {
+    public void testMatrixBFSOnALeaf() {
+        initMatrixGraph();
         List<Character> visit = bfs.visit('C');
         Assert.assertEquals(Arrays.asList('C'), visit);
     }
 
     @Test
-    public void testBFSOnAMiddleNode() {
+    public void testMatrixBFSOnAMiddleNode() {
+        initMatrixGraph();
         List<Character> visit = bfs.visit('E');
         Assert.assertEquals(Arrays.asList('E', 'D', 'F'), visit);
     }
 
     @Test
-    public void testBFSOnFullGraph() {
+    public void testMatrixBFSOnFullGraph() {
+        initMatrixGraph();
+        List<Character> visit = bfs.visit('A');
+        Assert.assertEquals(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F'), visit);
+    }
+
+    private void initAdjacencyGraph() {
+        Map<Character, List<IEdge<Character>>> mapGraph = new HashMap<>();
+        mapGraph.put('A', new LinkedList<>(Arrays.asList(
+                new WeightedDirectedEdge<>('A', 'B', 10),
+                new WeightedDirectedEdge<>('A', 'C', 10)
+        )));
+        mapGraph.put('B', new LinkedList<>(Arrays.asList(
+                new WeightedDirectedEdge<>('B', 'D', 20),
+                new WeightedDirectedEdge<>('B', 'E', 5)
+        )));
+        mapGraph.put('E', new LinkedList<>(Arrays.asList(
+                new WeightedDirectedEdge<>('E', 'D', 7),
+                new WeightedDirectedEdge<>('E', 'F', 10)
+        )));
+        mapGraph.put('F', new LinkedList<>(Arrays.asList(
+                new WeightedDirectedEdge<>('F', 'D', 2)
+        )));
+        Character[] nodes = new Character[]{'A', 'B', 'C', 'D', 'E', 'F'};
+        bfs = new BFS(mapGraph, nodes);
+    }
+
+    @Test
+    public void testAdjacencyBFSOnALeaf() {
+        initAdjacencyGraph();
+        List<Character> visit = bfs.visit('C');
+        Assert.assertEquals(Arrays.asList('C'), visit);
+    }
+
+    @Test
+    public void testAdjacencyBFSOnAMiddleNode() {
+        initAdjacencyGraph();
+        List<Character> visit = bfs.visit('E');
+        Assert.assertEquals(Arrays.asList('E', 'D', 'F'), visit);
+    }
+
+    @Test
+    public void testAdjacencyBFSOnFullGraph() {
+        initAdjacencyGraph();
         List<Character> visit = bfs.visit('A');
         Assert.assertEquals(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F'), visit);
     }

7881de922e4506f5edccc43a4e6b71bb10c25f8e
luigidb <luigi.debianchi@gmail.com>
2019-05-14 02:16:34 +0200

    Topological sort

--- src/main/java/utils/graph/algorithm/TopologicalSort.java
+++ src/main/java/utils/graph/algorithm/TopologicalSort.java
@@ -1,4 +1,47 @@
 package utils.graph.algorithm;
 
-public class TopologicalSort {
+import utils.graph.egde.IEdge;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TopologicalSort<T> {
+
+    private final List<IEdge<T>> edges;
+    private final T[] nodes;
+    private final Map<T, List<IEdge<T>>> graph;
+
+    public TopologicalSort(List<IEdge<T>> edges, T[] nodes) {
+        this.edges = edges;
+        this.nodes = nodes;
+        graph = edges.stream().collect(Collectors.groupingBy(
+                IEdge::getNodeS,
+                Collectors.mapping(i -> i, Collectors.toList())
+        ));
+    }
+
+    public List<T> sort() {
+        LinkedList<T> result = new LinkedList<>();
+        Set<T> toBeVisitedNodes = new HashSet<>(Arrays.asList(nodes));
+
+        while (!toBeVisitedNodes.isEmpty()) {
+            T anyNode = toBeVisitedNodes.stream().findAny().get();
+
+            visit(anyNode, result, toBeVisitedNodes);
+        }
+        return result;
+    }
+
+    private void visit(T node, LinkedList<T> result, Set<T> toBeVisitedNodes) {
+        toBeVisitedNodes.remove(node);
+
+        List<IEdge<T>> nextNodes = graph.get(node);
+        if (nextNodes != null)
+            for (IEdge<T> edge : nextNodes) {
+                if (toBeVisitedNodes.contains(edge.getNodeF())) {
+                    visit(edge.getNodeF(), result, toBeVisitedNodes);
+                }
+            }
+        result.addFirst(node);
+    }
 }
---
+++ src/test/java/utils/graph/algorithm/TopologicalSortTest.java
@@ -0,0 +1,37 @@
+package utils.graph.algorithm;
+
+import org.junit.Assert;
+import org.junit.Test;
+import utils.graph.egde.IWeightedEdge;
+import utils.graph.egde.directed.WeightedDirectedEdge;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TopologicalSortTest {
+
+    @Test
+    public void validateTopologicalSort() {
+        List<IWeightedEdge<Character>> edgeList = new LinkedList<>();
+        edgeList.add(new WeightedDirectedEdge<>('A', 'B', 10));
+        edgeList.add(new WeightedDirectedEdge<>('A', 'C', 10));
+        edgeList.add(new WeightedDirectedEdge<>('B', 'D', 20));
+        edgeList.add(new WeightedDirectedEdge<>('B', 'E', 5));
+        edgeList.add(new WeightedDirectedEdge<>('E', 'D', 7));
+        edgeList.add(new WeightedDirectedEdge<>('E', 'F', 10));
+        edgeList.add(new WeightedDirectedEdge<>('F', 'D', 2));
+        Character[] nodes = new Character[]{'A', 'B', 'C', 'D', 'E', 'F'};
+
+        TopologicalSort topologicalSort = new TopologicalSort(edgeList, nodes);
+        List sort = topologicalSort.sort();
+
+        Assert.assertTrue(sort.indexOf('A')<sort.indexOf('B'));
+        Assert.assertTrue(sort.indexOf('A')<sort.indexOf('C'));
+        Assert.assertTrue(sort.indexOf('B')<sort.indexOf('D'));
+        Assert.assertTrue(sort.indexOf('B')<sort.indexOf('E'));
+        Assert.assertTrue(sort.indexOf('E')<sort.indexOf('D'));
+        Assert.assertTrue(sort.indexOf('E')<sort.indexOf('F'));
+        Assert.assertTrue(sort.indexOf('F')<sort.indexOf('D'));
+    }
+
+}

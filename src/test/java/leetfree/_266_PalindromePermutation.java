1488620b7830c612f02e0605ffed131c6dde47c0
luigidb <luigi.debianchi@gmail.com>
2019-06-22 20:24:35 +0200

    Comments

--- src/test/java/leetfree/_266_PalindromePermutation.java
+++ src/test/java/leetfree/_266_PalindromePermutation.java
@@ -1,8 +1,12 @@
 package leetfree;
 
 public class _266_PalindromePermutation {
-    /*Given a string, determine if a permutation of the string could form a palindrome.
+    /**Given a string, determine if a permutation of the string could form a palindrome.
 
     For example,
     "code" -> False, "aab" -> True, "carerac" -> True.*/
+
+    /**
+     * Simply count the occurrences of each letter; sum all the even occurrences and, if exist, add one odd char .
+     */
 }

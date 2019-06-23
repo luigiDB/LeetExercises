b6d2eab5e60cdc3781bb7117b83cecb12394b98e
luigidb <luigi.debianchi@gmail.com>
2019-06-23 18:44:03 +0200

    Rabin Karp

---
+++ src/main/java/utils/pattern/RabinKarp.java
@@ -0,0 +1,51 @@
+package utils.pattern;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RabinKarp {
+    /**
+     * https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/
+     * hash(s+1 .. s+m) =
+     *      ( ( d *( hash(s .. s+m-1) - txt[s] * h ) ) + txt[s+m] ) mod q
+     * where
+     *      d -> alpahbet size
+     *      h -> d ^ pattern_size -1
+     *      q -> a prime number
+     */
+
+    public static List<Integer> search(String text, String pattern) {
+        int alphabetSize = 26;
+        int prime = 13;
+        int hashvalue = 1;
+
+        for (int i = 0; i < pattern.length() - 1; i++)
+            hashvalue = (hashvalue * alphabetSize) % prime;
+
+        int tHash = 0;
+        int pHash = 0;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            tHash = ((alphabetSize * tHash) + text.charAt(i)) % prime;
+            pHash = ((alphabetSize * pHash) + pattern.charAt(i)) % prime;
+        }
+
+        List<Integer> matches = new LinkedList<>();
+
+        for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+            if (tHash == pHash) {
+                int j;
+                for (j = 0; j < pattern.length(); j++) {
+                    if (text.charAt(i + j) != pattern.charAt(j))
+                        break;
+                }
+                if (j == pattern.length())
+                    matches.add(i);
+            }
+            if (i < text.length() - pattern.length())
+                tHash = (alphabetSize * (tHash - (text.charAt(i) * hashvalue)) + text.charAt(i + pattern.length())) % prime;
+        }
+
+        return matches;
+    }
+}

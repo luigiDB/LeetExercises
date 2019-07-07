66466c58105fcc592c4ff8250171347e09ba41b8
luigidb <luigi.debianchi@gmail.com>
2019-07-07 20:54:48 +0200

    Misc

---
+++ src/main/java/utils/pattern/LongestCommonSubsequence.java
@@ -0,0 +1,15 @@
+package utils.pattern;
+
+/**
+ * https://en.wikipedia.org/wiki/Longest_common_substring_problem
+ * Must be resolved for 2 and multiple strings
+ */
+
+/**
+ * Two possible techniques:
+ * + build the tree with all the strings and determine the deepest node with all the strings compare
+ * + buld the LCS matrix 2 string at the time and merge results
+ */
+public class LongestCommonSubsequence {
+    //TODO
+}
---
+++ src/test/java/geeksForGeek/AnagramSubstringSearch.java
@@ -0,0 +1,23 @@
+package geeksForGeek;
+
+/**
+ * https://www.geeksforgeeks.org/anagram-substring-search-search-permutations/
+ * Anagram Substring Search (Or Search for all permutations)
+ * Given a text txt[0..n-1] and a pattern pat[0..m-1], write a function search(char pat[], char txt[]) that prints all
+ * occurrences of pat[] and its permutations (or anagrams) in txt[]. You may assume that n > m.
+ * Expected time complexity is O(n)
+ *
+ * Examples:
+ *
+ * 1) Input:  txt[] = "BACDGABCDA"  pat[] = "ABCD"
+ *    Output:   Found at Index 0
+ *              Found at Index 5
+ *              Found at Index 6
+ * 2) Input: txt[] =  "AAABABAA" pat[] = "AABA"
+ *    Output:   Found at Index 0
+ *              Found at Index 1
+ *              Found at Index 4
+ */
+public class AnagramSubstringSearch {
+    //TODO
+}

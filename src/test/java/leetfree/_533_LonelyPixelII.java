f73f0122392a88685535f66957c99df7694597b5
luigidb <luigi.debianchi@gmail.com>
2019-07-18 01:33:37 +0200

    533

--- src/test/java/leetfree/_533_LonelyPixelII.java
+++ src/test/java/leetfree/_533_LonelyPixelII.java
@@ -1,35 +1,98 @@
 package leetfree;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Given a picture consisting of black and white pixels, and a positive integer N, find the number of black pixels located at some specific row R and column C that align with all the following rules:
+ * <p>
+ * Row R and column C both contain exactly N black pixels.
+ * For all rows that have a black pixel at column C, they should be exactly the same as row R
+ * The picture is represented by a 2D char array consisting of 'B' and 'W', which means black and white pixels respectively.
+ * <p>
+ * Example:
+ * Input:
+ * [['W', 'B', 'W', 'B', 'B', 'W'],
+ * ['W', 'B', 'W', 'B', 'B', 'W'],
+ * ['W', 'B', 'W', 'B', 'B', 'W'],
+ * ['W', 'W', 'B', 'W', 'B', 'W']]
+ * <p>
+ * N = 3
+ * Output: 6
+ * Explanation: All the bold 'B' are the black pixels we need (all 'B's at column 1 and 3).
+ * 0    1    2    3    4    5         column index
+ * 0    [['W', 'B', 'W', 'B', 'B', 'W'],
+ * 1     ['W', 'B', 'W', 'B', 'B', 'W'],
+ * 2     ['W', 'B', 'W', 'B', 'B', 'W'],
+ * 3     ['W', 'W', 'B', 'W', 'B', 'W']]
+ * row index
+ * <p>
+ * Take 'B' at row R = 0 and column C = 1 as an example:
+ * Rule 1, row R = 0 and column C = 1 both have exactly N = 3 black pixels.
+ * Rule 2, the rows have black pixel at column C = 1 are row 0, row 1 and row 2. They are exactly the same as row R = 0.
+ * <p>
+ * Note:
+ * The range of width and height of the input 2D array is [1,200].
+ */
 public class _533_LonelyPixelII {
-    /**
-     * Given a picture consisting of black and white pixels, and a positive integer N, find the number of black pixels located at some specific row R and column C that align with all the following rules:
-     *
-     * Row R and column C both contain exactly N black pixels.
-     * For all rows that have a black pixel at column C, they should be exactly the same as row R
-     * The picture is represented by a 2D char array consisting of 'B' and 'W', which means black and white pixels respectively.
-     *
-     * Example:
-     * Input:
-     * [['W', 'B', 'W', 'B', 'B', 'W'],
-     *  ['W', 'B', 'W', 'B', 'B', 'W'],
-     *  ['W', 'B', 'W', 'B', 'B', 'W'],
-     *  ['W', 'W', 'B', 'W', 'B', 'W']]
-     *
-     * N = 3
-     * Output: 6
-     * Explanation: All the bold 'B' are the black pixels we need (all 'B's at column 1 and 3).
-     *         0    1    2    3    4    5         column index
-     * 0    [['W', 'B', 'W', 'B', 'B', 'W'],
-     * 1     ['W', 'B', 'W', 'B', 'B', 'W'],
-     * 2     ['W', 'B', 'W', 'B', 'B', 'W'],
-     * 3     ['W', 'W', 'B', 'W', 'B', 'W']]
-     * row index
-     *
-     * Take 'B' at row R = 0 and column C = 1 as an example:
-     * Rule 1, row R = 0 and column C = 1 both have exactly N = 3 black pixels.
-     * Rule 2, the rows have black pixel at column C = 1 are row 0, row 1 and row 2. They are exactly the same as row R = 0.
-     *
-     * Note:
-     * The range of width and height of the input 2D array is [1,200].
-     */
+
+    @Test
+    public void testProvidedScenario() {
+        char[][] matrix = new char[][]{
+                {'W', 'B', 'W', 'B', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'B', 'W'},
+                {'W', 'W', 'B', 'W', 'B', 'W'},
+        };
+        Assert.assertEquals(6, countLonely(matrix, 3));
+    }
+
+    @Test
+    public void testBaseScenario() {
+        char[][] matrix = new char[][]{
+                {'B', 'W', 'W'},
+                {'W', 'W', 'W'},
+                {'W', 'W', 'W'}
+        };
+        Assert.assertEquals(1, countLonely(matrix, 1));
+    }
+
+    @Test
+    public void testMinimalValidScenario() {
+        char[][] matrix = new char[][]{
+                {'B', 'B', 'W'},
+                {'W', 'B', 'B'},
+                {'W', 'W', 'W'}
+        };
+        Assert.assertEquals(2, countLonely(matrix, 2));
+    }
+
+    private int countLonely(char[][] matrix, int hitCount) {
+        int[] rowCount = new int[matrix.length];
+        int[] columnCount = new int[matrix[0].length];
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 'B') {
+                    rowCount[i]++;
+                    columnCount[j]++;
+                }
+            }
+        }
+        int lonelyCounter = 0;
+
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (columnCount[j] == hitCount) {
+                for (int i = 0; i < matrix.length; i++) {
+                    if (matrix[i][j] == 'B') {
+                        if (rowCount[i] == hitCount)
+                            lonelyCounter++;
+                    }
+                }
+            }
+        }
+
+        return lonelyCounter;
+    }
 }

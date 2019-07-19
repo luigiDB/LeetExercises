9517e7ca94a0745af6d9cb1c932187788c5a2106
luigidb <luigi.debianchi@gmail.com>
2019-07-19 02:14:49 +0200

    545 better solution but still uses a global variable

--- src/test/java/leetfree/_545_BoundaryOfBinaryTree.java
+++ src/test/java/leetfree/_545_BoundaryOfBinaryTree.java
@@ -3,31 +3,29 @@ package leetfree;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Given a binary tree, return the values of its boundary in anti-clockwise direction starting from root. Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes.
- *
+ * <p>
  * Left boundary is defined as the path from root to the left-most node. Right boundary is defined as the path from root to the right-most node. If the root doesn't have left subtree or right subtree, then the root itself is left boundary or right boundary. Note this definition only applies to the input binary tree, and not applies to any subtrees.
- *
+ * <p>
  * The left-most node is defined as a leaf node you could reach when you always firstly travel to the left subtree if exists. If not, travel to the right subtree. Repeat until you reach a leaf node.
- *
+ * <p>
  * The right-most node is also defined by the same way with left and right exchanged.
- *
+ * <p>
  * Example 1
  * Input:
- *   1
- *    \
- *     2
- *    / \
- *   3   4
- *
+ * 1
+ * \
+ * 2
+ * / \
+ * 3   4
+ * <p>
  * Ouput:
  * [1, 3, 4, 2]
- *
+ * <p>
  * Explanation:
  * The root doesn't have left subtree, so the root itself is left boundary.
  * The leaves are node 3 and 4.
@@ -35,17 +33,17 @@ import java.util.stream.Collectors;
  * So order them in anti-clockwise without duplicates and we have [1,3,4,2].
  * Example 2
  * Input:
- *     ____1_____
- *    /          \
- *   2            3
- *  / \          /
+ * ____1_____
+ * /          \
+ * 2            3
+ * / \          /
  * 4   5        6
- *    / \      / \
- *   7   8    9  10
- *
+ * / \      / \
+ * 7   8    9  10
+ * <p>
  * Ouput:
  * [1,2,4,7,8,9,10,6,3]
- *
+ * <p>
  * Explanation:
  * The left boundary are node 1,2,4. (4 is the left-most node according to definition)
  * The leaves are node 4,7,8,9,10.
@@ -93,50 +91,42 @@ public class _545_BoundaryOfBinaryTree {
 
         tree.setRight(tmp2);
 
-        Assert.assertEquals(Arrays.asList(1,2,4,7,8,9,10,6,3), treePerimeter(tree));
+        Assert.assertEquals(Arrays.asList(1, 2, 4, 7, 8, 9, 10, 6, 3), treePerimeter(tree));
     }
 
     private List<Integer> treePerimeter(Node tree) {
         List<Node> visitOrder = new LinkedList<>();
-        visitOrder.add(tree);
-
-        Node tmp = tree;
-        while (tmp.getLeft() != null) {
-            tmp = tmp.getLeft();
-            visitOrder.add(tmp);
-        }
-
-        List<Node> leaves = new LinkedList<>();
-        getAllLeaves(tree, leaves);
-
-        leaves = leaves.stream().filter(l -> !visitOrder.contains(l)).collect(Collectors.toList());
-        visitOrder.addAll(leaves);
-
-        tmp = tree;
-        leaves  = new LinkedList<>();
-        while(tmp.getLeft() != null || tmp.getRight() != null) {
-            if (tmp.getRight() != null)
-                tmp = tmp.getRight();
-            else
-                tmp = tmp.getLeft();
-            leaves.add(0, tmp);
-        }
-
-        leaves = leaves.stream().filter(l -> !visitOrder.contains(l)).collect(Collectors.toList());
-        visitOrder.addAll(leaves);
+        LinkedList<Node> path = new LinkedList<>();
+        getAllLeaves(tree, visitOrder, path);
+        Collections.reverse(lastPath);
+        visitOrder.addAll(lastPath);
 
         return visitOrder.stream().map(Node::getValue).collect(Collectors.toList());
     }
 
-    private void getAllLeaves(Node node, List<Node> leaves) {
-        if(node.getLeft() == null && node.getRight() == null)
-            leaves.add(node);
+    private List<Node> lastPath = new LinkedList<>();
 
-        if(node.getLeft() != null)
-            getAllLeaves(node.getLeft(), leaves);
+    private void getAllLeaves(Node node, List<Node> leaves, LinkedList<Node> path) {
+        path.add(node);
+        if (node.getLeft() == null && leaves.size() == 0) {
+            leaves.addAll(path);
+        }
 
-        if(node.getRight() != null)
-            getAllLeaves(node.getRight(), leaves);
+        if (node.getLeft() == null && node.getRight() == null) {
+            if (node != leaves.get(leaves.size() - 1))
+                leaves.add(node);
+            lastPath = (path.size()>1)?new LinkedList<>(path.subList(1, path.size()-1)):new LinkedList<>();
+        }
+
+        if (node.getLeft() != null) {
+            getAllLeaves(node.getLeft(), leaves, path);
+            path.removeLast();
+        }
+
+        if (node.getRight() != null) {
+            getAllLeaves(node.getRight(), leaves, path);
+            path.removeLast();
+        }
     }
 
     class Node {

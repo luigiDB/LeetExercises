876e682334d8534b87b925d3a82df08612adc5ba
luigidb <luigi.debianchi@gmail.com>
2019-08-03 13:23:06 +0200

    min heap solution

--- src/main/java/utils/subarray/K_thLargestSumContiguousSubarray.java
+++ src/main/java/utils/subarray/K_thLargestSumContiguousSubarray.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 public class K_thLargestSumContiguousSubarray {
 
     static public int find(int[] input, int k) {
-        PriorityQueue<Integer> sums = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> sums = new PriorityQueue<>();
 
         int[] partialSum = new int[input.length + 1];
         for (int i = 1; i < input.length + 1; i++) {
@@ -14,14 +14,16 @@ public class K_thLargestSumContiguousSubarray {
 
         for (int i = 0; i < partialSum.length; i++) {
             for (int j = i + 1; j < partialSum.length; j++) {
-                sums.offer(partialSum[j] - partialSum[i]);
+                if(sums.size()<k)
+                    sums.offer(partialSum[j] - partialSum[i]);
+                else
+                    if(sums.peek()<partialSum[j] - partialSum[i]) {
+                        sums.poll();
+                        sums.offer(partialSum[j] - partialSum[i]);
+                    }
             }
         }
 
-        int res = 0;
-        for (int i = 0; i < k; i++) {
-            res = sums.poll();
-        }
-        return res;
+        return sums.poll();
     }
 }

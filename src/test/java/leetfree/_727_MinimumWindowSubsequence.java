d6111476ebc43613ec6fbdc179baf5235316b329
luigidb <luigi.debianchi@gmail.com>
2019-09-01 23:30:20 +0200

    Comments

--- src/test/java/leetfree/_727_MinimumWindowSubsequence.java
+++ src/test/java/leetfree/_727_MinimumWindowSubsequence.java
@@ -39,6 +39,7 @@ public class _727_MinimumWindowSubsequence {
         /**
          * in this case the table should be like
          *
+         *      a   b   c   d   e   b   d   d   e
          *      0   1   2   3   4   5   6   7   8   <-- index
          *  b   1   1   5   5   5   5   -1  -1  -1
          *  d   3   3   3   3   6   6   6   7   -1
@@ -46,8 +47,11 @@ public class _727_MinimumWindowSubsequence {
          *  ^
          *  |
          *  letter
-         *  Since we are interested only in the min string we can simply follow teh first vaslid path for each
-         *  ocurrence of the first char.
+         *  The above structure can be build in linear time starting from the end.
+         *  Since we are interested only in the min string we need to follow all valid path from first line to last
+         *  line. In this case are valid paths:
+         *      1 -> 3 -> 4
+         *      5 -> 6 -> 8
          */
     }
 

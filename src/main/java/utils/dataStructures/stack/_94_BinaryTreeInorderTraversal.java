8c7608f067642d5d1a68f49d617d35a1c030fb7d
luigidb <luigi.debianchi@gmail.com>
2019-10-20 16:50:12 +0200

    In order traversal with stack

--- src/main/java/utils/dataStructures/stack/_94_BinaryTreeInorderTraversal.java
+++ src/main/java/utils/dataStructures/stack/_94_BinaryTreeInorderTraversal.java
@@ -1,19 +1,43 @@
 package utils.dataStructures.stack;
 
-/**
- * Given a binary tree, return the inorder traversal of its nodes' values.
- *
- * Example:
- *
- * Input: [1,null,2,3]
- *    1
- *     \
- *      2
- *     /
- *    3
- *
- * Output: [1,3,2]
- * Follow up: Recursive solution is trivial, could you do it iteratively?
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+Example:
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
  */
 public class _94_BinaryTreeInorderTraversal {
+    public List<Integer> inOrderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
 }

a7988069cd5ae6cb785d0bebc78e314299ccfee8
luigidb <luigi.debianchi@gmail.com>
2019-11-04 08:55:28 +0100

    Disjoint set exercises

---
+++ src/test/java/leetfree/__200_NumberOfIslands.java
@@ -0,0 +1,28 @@
+package leetfree;
+
+/*
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and
+is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are
+ all surrounded by water.
+Example 1:
+Input:
+11110
+11010
+11000
+00000
+Output: 1
+Example 2:
+Input:
+11000
+11000
+00100
+00011
+Output: 3
+ */
+public class __200_NumberOfIslands {
+    /**
+     * The idea to solve it is to traverse the matrix and when we encounter an island we color all the connected land
+     * counting the number of colors.
+     * Or we can use the disjointSet approach.
+     */
+}

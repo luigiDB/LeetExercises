89039b2e0d504bd2666cda6b3e5790111db2058e
luigidb <luigi.debianchi@gmail.com>
2019-11-04 08:45:32 +0100

    Improved Trienode class

--- src/main/java/utils/tree/TrieNode.java
+++ src/main/java/utils/tree/TrieNode.java
@@ -1,23 +1,18 @@
 package utils.tree;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class TrieNode<T> {
 
     T info;
-    List<T> sons;
+    Set<T> sons;
 
     public TrieNode(T info) {
         this.info = info;
-        sons = null;
+        sons = new HashSet<>();
     }
 
     public void addSon(T son) {
-        if(sons!=null)
-            sons.add(son);
-        else
-            sons = new LinkedList<>(Arrays.asList(son));
+        sons.add(son);
     }
 }

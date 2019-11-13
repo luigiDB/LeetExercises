25acb83418069a9eece5a2fd5235691e332cb318
luigidb <luigi.debianchi@gmail.com>
2019-11-13 22:58:27 +0100

    Implemented some old exercises solution

--- src/test/java/leetfree/_259_3SumSmaller.java
+++ src/test/java/leetfree/_259_3SumSmaller.java
@@ -1,5 +1,7 @@
 package leetfree;
 
+import java.util.Arrays;
+
 /***
  * Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n
  * that satisfy the condition nums[i] + nums[j] + nums[k] < target.
@@ -14,5 +16,51 @@ package leetfree;
  *     Could you solve it in O(n2) runtime?
  */
 public class _259_3SumSmaller {
-    //TODO
+    /**
+     * Let us try sorting the array first. For example, nums = [3,5,2,8,1]nums=[3,5,2,8,1] becomes [1,2,3,5,8][1,2,3,5,8].
+     * Let us look at an example nums = [1,2,3,5,8]nums=[1,2,3,5,8], and target = 7target=7.
+     * [1, 2, 3, 5, 8]
+     *  ↑           ↑
+     * left       right
+     * Let us initialize two indices, leftleft and rightright pointing to the first and last element respectively.
+     * When we look at the sum of first and last element, it is 1 + 8 = 91+8=9, which is \geq target≥target. That tells
+     * us no index pair will ever contain the index rightright. So the next logical step is to move the right pointer
+     * one step to its left.
+     * [1, 2, 3, 5, 8]
+     *  ↑        ↑
+     * left    right
+     * Now the pair sum is 1 + 5 = 61+5=6, which is < target<target. How many pairs with one of the index = left that
+     * satisfy the condition? You can tell by the difference between rightright and leftleft which is 33, namely (1,2),
+     * (1,3) and (1,5). Therefore, we move left one step to its right.
+     */
+
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            sum += twoSumSmaller(nums, i + 1, target - nums[i]);
+        }
+        return sum;
+    }
+
+    private int twoSumSmaller(int[] nums, int startIndex, int target) {
+        int sum = 0;
+        int left = startIndex;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                sum += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * Time complexity : O(n^2). The twoSumSmaller function takes O(n) time because both left and right traverse at
+     * most n steps. Therefore, the overall time complexity is O(n^2).
+     * Space complexity : O(1).
+     */
 }
--- src/test/java/leetfree/_272_ClosestBinarySearchTreeValueII.java
+++ src/test/java/leetfree/_272_ClosestBinarySearchTreeValueII.java
@@ -1,13 +1,53 @@
 package leetfree;
 
-public class _272_ClosestBinarySearchTreeValueII {
-    /*
-    Given a non-empty binary search tree and a target value, find k values in the BST that are closest to the target.
+import utils.tree.visit.Tree;
 
-    Note:
-    Given target value is a floating point.
-    You may assume k is always valid, that is: k ≤ total nodes.
-    You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
-    Follow up:
-    Assume that the BST is balanced, could you solve it in less than O(n) runtime (where n = total nodes)?*/
+import java.util.*;
+
+/*
+Given a non-empty binary search tree and a target value, find k values in the BST that are closest to the target.
+Note:
+Given target value is a floating point.
+You may assume k is always valid, that is: k ≤ total nodes.
+You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
+Follow up:
+Assume that the BST is balanced, could you solve it in less than O(n) runtime (where n = total nodes)?
+*/
+public class _272_ClosestBinarySearchTreeValueII {
+
+    /**
+     *  In practise we keep a max heap of the closest values this way the space overhead is just K and the time
+     *  complexity is N since we simply read all the tree.
+     */
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+
+        Queue<TreeNode> bfsQueue = new LinkedList<>();
+        while (!bfsQueue.isEmpty()) {
+            TreeNode poll = bfsQueue.poll();
+            if (maxHeap.size() < k)
+                maxHeap.offer(poll.val);
+            else {
+                if (Math.abs(target - maxHeap.peek()) > Math.abs(target - poll.val)) {
+                    maxHeap.poll();
+                    maxHeap.offer(poll.val);
+                }
+            }
+            bfsQueue.add(poll.left);
+            bfsQueue.add(poll.right);
+        }
+
+        return new ArrayList<>(maxHeap);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
 }

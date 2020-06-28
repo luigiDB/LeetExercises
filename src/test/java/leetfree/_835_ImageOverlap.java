e8cbc8e1a4bd30046295b9e0e294570361543f47
luigidb <luigi.debianchi@gmail.com>
2020-06-28 18:56:51 +0200

    835

---
+++ src/test/java/leetfree/_835_ImageOverlap.java
@@ -0,0 +1,37 @@
+package leetfree;
+
+/*
+Two images A and B are given, represented as binary, square matrices of the same size.  (A binary matrix has only 0s
+and 1s as values.)
+We translate one image however we choose (sliding it left, right, up, or down any number of units), and place it on
+top of the other image.  After, the overlap of this translation is the number of positions that have a 1 in both images.
+(Note also that a translation does not include any kind of rotation.)
+What is the largest possible overlap?
+Example 1:
+Input: A = [[1,1,0],
+            [0,1,0],
+            [0,1,0]]
+       B = [[0,0,0],
+            [0,1,1],
+            [0,0,1]]
+Output: 3
+Explanation: We slide A to right by 1 unit and down by 1 unit.
+Notes:
+1 <= A.length = A[0].length = B.length = B[0].length <= 30
+0 <= A[i][j], B[i][j] <= 1
+ */
+public class _835_ImageOverlap {
+
+    /*TODO: There are no really efficient way to resolve this; apart from knowing the Fourier Transform.
+    The best we can obtain is N^4 time complexity like
+    for any point in A
+        for any point in B
+            evaluate the delta and keep track of how many points participate in each delta
+
+    return the delta with the highest number of occurrences.
+     */
+    public int largestOverlap(int[][] A, int[][] B) {
+        return 0;
+    }
+
+}

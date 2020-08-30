bb04e262571eb725ba3cf1bfc40c7f3baa931e23
luigidb <luigi.debianchi@gmail.com>
2020-08-30 12:46:12 +0200

    Tarjan and sorting references

---
+++ src/main/java/utils/graph/connectedComponents/TarjanAlgorithm.java
@@ -0,0 +1,10 @@
+package utils.graph.connectedComponents;
+
+/**
+ *  Linear time algorithm for finding the bridges in a graph was described by Robert Tarjan in 1974.
+ *  For implementation follow
+ *  {@link leetfree._1192_CriticalConnectionsInANetwork}
+ */
+public class TarjanAlgorithm {
+
+}

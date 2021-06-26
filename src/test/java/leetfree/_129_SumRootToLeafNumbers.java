e90d21e3748549e4322b4ec122f61bb8816e173d
luigidb <luigi.debianchi@gmail.com>
2021-06-26 14:08:44 +0200

    es

---
+++ src/test/java/leetfree/_129_SumRootToLeafNumbers.java
@@ -0,0 +1,85 @@
+package leetfree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * You are given the root of a binary tree containing digits from 0 to 9 only.
+ * Each root-to-leaf path in the tree represents a number.
+ * For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+ * Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a
+ * 32-bit integer.
+ * A leaf node is a node with no children.
+ */
+public class _129_SumRootToLeafNumbers {
+
+    @Test
+    public void a() {
+        TreeNode root = new TreeNode(
+                1,
+                new TreeNode(2),
+                new TreeNode(3)
+        );
+        assertEquals(25, sumNumbers(root));
+    }
+
+    @Test
+    public void b() {
+        TreeNode root = new TreeNode(
+                4,
+                new TreeNode(9,
+                        new TreeNode(5),
+                        new TreeNode(1)
+                ),
+                new TreeNode(0)
+        );
+        assertEquals(1026, sumNumbers(root));
+    }
+
+    int sum = 0;
+
+    public int sumNumbers(TreeNode root) {
+        sum = 0;
+
+        searchLeavesWithPath(root, 0);
+
+        return sum;
+    }
+
+    private void searchLeavesWithPath(TreeNode root, int number) {
+
+        if (root == null)
+            return;
+
+        int numberSoFar = number * 10 + root.val;
+
+        if(root.left == null & root.right==null) {
+            sum += numberSoFar;
+            return;
+        }
+
+        searchLeavesWithPath(root.left, numberSoFar);
+        searchLeavesWithPath(root.right, numberSoFar);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
